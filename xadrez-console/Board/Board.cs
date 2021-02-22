25bd34ac038659aec2d808e8b3592a576c40280c
Felipe <felipe.silva.hts@gmail.com>
2021-02-22 11:40:11 -0300

    Implementação Tabuleiro iniciada

---
+++ xadrez-console/Board/Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace board
+{
+    class Board
+    {
+        public int Lines { get; set; }
+        public int Columns { get; set; }
+
+        private Piece[,] Pieces { get; set; }
+
+        public Board(int lines, int columns)
+        {
+            Lines = lines;
+            Columns = columns;
+            Pieces = new Piece[lines,columns];
+        }
+    }
+}
---
+++ xadrez-console/Board/Piece.cs
@@ -0,0 +1,18 @@
+﻿namespace board
+{
+    class Piece
+    {
+        public Position _Position { get; set; }
+        public Color _Color { get; protected set; }
+        public int MovesAmount { get; protected set; }
+        public Board _Board { get; protected set; }
+
+        public Piece(Position position, Color color, Board board)
+        {
+            _Position = position;
+            _Color = color;
+            _Board = board;
+            MovesAmount = 0;
+        }
+    }
+}
